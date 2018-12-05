16c86ac297c1d36835f14c83fddbcd983fc3a903
Alexander Kelman <alexander.kelman@cgi.com>
2018-12-05 07:00:53 +0100

    Day05

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -10,15 +10,61 @@ namespace Day05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 5 Part 1:" + PartOne());
+			var input = System.IO.File.ReadAllText("input.txt");
+
+			Console.WriteLine("Day 5 Part 1:" + PartOne(input));
+            Console.WriteLine("Day 5 Part 2:" + PartTwo(input));
 
             Console.ReadKey();
+		}
+
+        private static int PartOne(string input)
+        {
+			return CalculatePolymerLength(input);
         }
 
-        private static string PartOne()
-        {
-            var input = System.IO.File.ReadAllLines("input.txt");
-            return "";
-        }
+		private static int CalculatePolymerLength(string polymer)
+		{
+			var input = polymer;
+			var foundCaseToRemove = true;
+
+			while (foundCaseToRemove)
+			{
+				foundCaseToRemove = false;
+				for (var i = 0; i < input.Length - 1; i++)
+				{
+					if (string.Equals(input[i].ToString(), input[i+1].ToString(), StringComparison.OrdinalIgnoreCase))
+					{
+						if (input[i] != input[i+1])
+						{
+							foundCaseToRemove = true;
+							input = input.Remove(i, 2);
+							break;
+						}
+					}
+				}
+			}
+			return input.Length;
+		}
+
+		private static int PartTwo(string input)
+		{
+			var shortest = int.MaxValue;
+
+			var usedCharacters = input.Select(x => char.ToUpper(x)).Distinct().ToArray();
+
+			foreach (var character in usedCharacters)
+			{
+				var removedCharacterInput = input.Replace(character.ToString().ToUpper(), "").Replace(character.ToString().ToLower(), "").Trim();
+				var length = CalculatePolymerLength(removedCharacterInput);
+
+				if (length < shortest)
+				{
+					shortest = length;
+				}
+			}
+
+			return shortest;
+		}
     }
 }
