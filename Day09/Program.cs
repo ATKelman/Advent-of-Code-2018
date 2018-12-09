de63cc1e8b99e0f037f254b5e923d18ba65495fb
Alexander Kelman <alexanderkelman95@hotmail.com>
2018-12-09 15:59:15 +0100

    Day 9 part 1 and 2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -8,47 +8,37 @@ namespace Day09
 {
     class Program
     {
-        const int PLAYERS = 10;
-        const int MARBLES = 1618 * 100;
+        const int PLAYERS = 412;
+        const int MARBLES = 71646;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 9 Part 1: " + PartOne());
+            Console.WriteLine("Day 9 Part 1: " + CalculateScore(PLAYERS, MARBLES));
+            Console.WriteLine("Day 9 Part 1: " + CalculateScore(PLAYERS, MARBLES * 100));
 
             Console.ReadKey();
         }
 
-        // Last marble = 71646 
-        private static long PartOne()
+        private static long CalculateScore(int players, int marbles)
         {
-            var scores = new long[PLAYERS];
-            var circle = new LinkedList<long>();
+            var scores = new long[players];
+            var circle = new LinkedList<int>();
             var current = circle.AddFirst(0);
 
-            var answerFound = false;
-            for (int i = 1; i < MARBLES; i++)
+            for (int i = 1; i < marbles; i++)
             {
-                if (answerFound)
-                {
-                    break;
-                }
                 if (i % 23 == 0)
                 {
                     for (int j = 0; j < 7; j++)
                     {
                         current = current.Previous ?? circle.Last;
-                        scores[i % PLAYERS] += (i + current.Value);
-                        if ((i + current.Value) == (MARBLES / 100))
-                        {
-                            answerFound = true;
-                            break;
-                        }
-
-
-                        var tmp = current;
-                        current = current.Previous ?? circle.Last;
-                        circle.Remove(tmp);
                     }
+
+                    scores[i % PLAYERS] += (i + current.Value);
+
+                    var tmp = current;
+                    current = current.Next ?? circle.First;
+                    circle.Remove(tmp);
                 }
                 else
                 {
