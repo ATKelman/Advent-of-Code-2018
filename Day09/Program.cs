5933d54e32b73c6a7d1fa6e7149aff10633ca90b
Alexander Kelman <alexanderkelman95@hotmail.com>
2018-12-09 15:43:21 +0100

    Day 09 WIP

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -8,6 +8,9 @@ namespace Day09
 {
     class Program
     {
+        const int PLAYERS = 10;
+        const int MARBLES = 1618 * 100;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Day 9 Part 1: " + PartOne());
@@ -15,10 +18,45 @@ namespace Day09
             Console.ReadKey();
         }
 
-        private static int PartOne()
+        // Last marble = 71646 
+        private static long PartOne()
         {
+            var scores = new long[PLAYERS];
+            var circle = new LinkedList<long>();
+            var current = circle.AddFirst(0);
 
-            return 0;
+            var answerFound = false;
+            for (int i = 1; i < MARBLES; i++)
+            {
+                if (answerFound)
+                {
+                    break;
+                }
+                if (i % 23 == 0)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        current = current.Previous ?? circle.Last;
+                        scores[i % PLAYERS] += (i + current.Value);
+                        if ((i + current.Value) == (MARBLES / 100))
+                        {
+                            answerFound = true;
+                            break;
+                        }
+
+
+                        var tmp = current;
+                        current = current.Previous ?? circle.Last;
+                        circle.Remove(tmp);
+                    }
+                }
+                else
+                {
+                    current = circle.AddAfter(current.Next ?? circle.First, i);
+                }
+            }
+
+            return scores.Max();
         }
     }
 }
