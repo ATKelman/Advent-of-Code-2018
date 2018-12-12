e9197bdb95735e85a2c6baa2f33c849ff66c8b6b
Alexander Kelman <alexanderkelman95@hotmail.com>
2018-12-12 22:51:58 +0100

    Day 12 Part 1 and 2

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,6 @@ namespace Day12
         const string PLANTS = "#.#####.#.#.####.####.#.#...#.......##..##.#.#.#.###..#.....#.####..#.#######.#....####.#....##....#";
         static void Main(string[] args)
         {
-            
-
             Console.WriteLine("Day 12 Part 1 : " + PartOne());
 
             Console.ReadKey();
@@ -20,8 +19,7 @@ namespace Day12
 
         private static int PartOne()
         {
-            //var plants = "...." + "#.#####.#.#.####.####.#.#...#.......##..##.#.#.#.###..#.....#.####..#.#######.#....####.#....##....#";
-            var plants = "...#..#.#..##......###...###...........";
+            var plants = "#.#####.#.#.####.####.#.#...#.......##..##.#.#.#.###..#.....#.####..#.#######.#....####.#....##....#";
 
             var rules = System.IO.File.ReadAllLines("input.txt").Select(x =>
             {
@@ -34,25 +32,29 @@ namespace Day12
             }).ToList();
 
             var sum = 0;
-            for (int gen = 0; gen < 20; gen++)
+            var negativeSteps = 0;
+
+            var sumPrev = 0;
+            var sumCurrent = 0;
+            var sumNext = 0;
+
+            for (int gen = 1; gen <= 5000000000; gen++)
             {
                 var newPlants = "";
 
                 var firstHash = plants.IndexOf('#');
                 for (int start = 0; start < 4 - firstHash; start++)
                 {
-                    newPlants.Insert(0, ".");
+                    newPlants += ".";
+                    negativeSteps--;
                 }
 
                 newPlants += plants;
 
                 var lastHash = newPlants.LastIndexOf('#');
-                for (int end = 0; end < 5 - (newPlants.Length - lastHash); end++)
-                {
-                    newPlants += ".";
-                }
+                newPlants = newPlants.PadRight(lastHash + 5, '.');
 
-                var nextPlants = "";
+                var nextPlants = "..";
                 for (int i = 0; i < newPlants.Length - 5; i++)
                 {
                     var currentPlantCombo = newPlants.ToString().Substring(i, 5);
@@ -71,11 +73,49 @@ namespace Day12
                     }
                 }
 
-                sum += nextPlants.ToCharArray().Where(x => x == '#').Count();
+                // Difference Calculation for part 2
+                sumPrev = sumCurrent;
+                sumCurrent = sumNext;
+                sumNext = 0;
+                for (int i = 0; i < nextPlants.Length; i++)
+                {
+                    if (nextPlants[i] == '#')
+                    {
+                        sumNext += i + negativeSteps;
+                    }
+                }
+
+                if ((sumCurrent - sumPrev) == (sumNext - sumCurrent))
+                {
+                    PartTwo(gen, sumNext, (sumNext - sumCurrent));
+                }
+
                 plants = nextPlants;
+            }
+            
+            for (int i = 0; i < plants.Length ; i++)
+            {
+                if (plants[i] == '#')
+                {
+                    sum += i + negativeSteps;
+                }
             }
 
             return sum;
+        }
+
+        private static void PartTwo(int generationsPassed, int currentSum, int difference)
+        {
+            // Math done manually
+            //var i = 50000000000;
+            //var result = (423570 + (i - 7388) * 57);
+            //var test = result;
+
+            var i = 50000000000;
+            var result = (currentSum + (i - generationsPassed) * difference);
+            var test = result;
+
+            Console.WriteLine("Day 12 Part 2 : " + result);
         }
     }
 }
