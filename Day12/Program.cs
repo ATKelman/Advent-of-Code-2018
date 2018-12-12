dfaafcda083f1b2c86f1d0930a4d847710421efb
Alexander Kelman <alexanderkelman95@hotmail.com>
2018-12-12 07:00:53 +0100

    Day 12 WIP

---
+++ Day12/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    class Program
+    {
+        const string PLANTS = "#.#####.#.#.####.####.#.#...#.......##..##.#.#.#.###..#.....#.####..#.#######.#....####.#....##....#";
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Day 12 Part 1 : " + PartOne());
+
+            Console.ReadKey();
+        }
+
+        private static int PartOne()
+        {
+            //var plants = "...." + "#.#####.#.#.####.####.#.#...#.......##..##.#.#.#.###..#.....#.####..#.#######.#....####.#....##....#";
+            var plants = "...#..#.#..##......###...###...........";
+
+            var rules = System.IO.File.ReadAllLines("input.txt").Select(x =>
+            {
+                var split = x.Split(new[] { ' ', '=', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                return new
+                {
+                    Rule = split[0],
+                    Plant = split[1]
+                };
+            }).ToList();
+
+            var sum = 0;
+            for (int gen = 0; gen < 20; gen++)
+            {
+                var newPlants = "";
+
+                var firstHash = plants.IndexOf('#');
+                for (int start = 0; start < 4 - firstHash; start++)
+                {
+                    newPlants.Insert(0, ".");
+                }
+
+                newPlants += plants;
+
+                var lastHash = newPlants.LastIndexOf('#');
+                for (int end = 0; end < 5 - (newPlants.Length - lastHash); end++)
+                {
+                    newPlants += ".";
+                }
+
+                var nextPlants = "";
+                for (int i = 0; i < newPlants.Length - 5; i++)
+                {
+                    var currentPlantCombo = newPlants.ToString().Substring(i, 5);
+                    var ruleFound = false;
+                    rules.ForEach(x =>
+                    {
+                        if (x.Rule == currentPlantCombo)
+                        {
+                            nextPlants += x.Plant;
+                            ruleFound = true;
+                        }
+                    });
+                    if (!ruleFound)
+                    {
+                        nextPlants += ".";
+                    }
+                }
+
+                sum += nextPlants.ToCharArray().Where(x => x == '#').Count();
+                plants = nextPlants;
+            }
+
+            return sum;
+        }
+    }
+}
